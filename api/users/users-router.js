27313e0241e5edb475464caf02acfaf011005954
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-08-16 22:23:19 -0700

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,4 +1,9 @@
+const router = require("express").Router()
+const { restricted } = require("../auth/auth-middleware")
+
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
+
+
 
 
 /**
@@ -23,6 +28,13 @@
     "message": "You shall not pass!"
   }
  */
-
+router.get('/', restricted, async (req, res , next)=>{
+ try{
+  const users = await users.find()
+ } catch(err){
+   next(err)
+ }
+})
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router
